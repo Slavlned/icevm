25b889577d336081e10072b87673d88484c487d9
Vyacheslav <91011801+Slavlned@users.noreply.github.com>
2025-01-15 22:43:47 +0500

    sources

---
+++ Vm/Instructions/VmInstrThrow.java
@@ -0,0 +1,34 @@
+package com.slavlend.Vm.Instructions;
+
+import com.slavlend.Vm.*;
+import lombok.Getter;
+
+/*
+Выкидывает значение
+ */
+@Getter
+public class VmInstrThrow implements VmInstr {
+    // адресс
+    private final VmInAddr addr;
+
+    // конструктор
+    public VmInstrThrow(VmInAddr addr) {
+        this.addr = addr;
+    }
+
+    @Override
+    public void run(IceVm vm, VmFrame<Object> frame) {
+        Object o = vm.pop();
+        throw new VmThrowable(o);
+    }
+
+    @Override
+    public String toString() {
+        return "THROW()";
+    }
+
+    @Override
+    public void print() {
+        System.out.println(this);
+    }
+}
